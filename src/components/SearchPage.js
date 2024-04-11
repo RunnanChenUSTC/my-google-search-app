6343fe982480c7192cab127b2f011f20e20959fb
ChenRunnanUSTC <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-04-11 07:30:10 +0000

    fdfv

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -8,9 +8,27 @@ const SearchPage = () => {
         script.async = true;
         document.body.appendChild(script);
 
-        // 组件卸载时移除脚本
+        // 创建点击事件监听器
+        const clickListener = (event) => {
+            // 检查是否点击了搜索结果链接
+            if (event.target && event.target.matches("a.gs-title")) {
+                const url = event.target.href; // 获取链接的URL
+
+                // 使用gtag发送点击事件到Google Analytics
+                gtag('event', 'select_content', {
+                    content_type: 'search_result',
+                    item_id: url,
+                });
+            }
+        };
+
+        // 添加事件监听器到文档
+        document.addEventListener('click', clickListener);
+
+        // 组件卸载时移除脚本和事件监听器
         return () => {
             document.body.removeChild(script);
+            document.removeEventListener('click', clickListener);
         };
     }, []);
 
