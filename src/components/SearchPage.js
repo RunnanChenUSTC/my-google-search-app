cc1543e6c427599b2bb18ad6873cd7c60439b01f
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-04-14 21:42:27 +0800

    Update SearchPage.js

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,37 +2,42 @@ import React, { useEffect } from 'react';
 
 const SearchPage = () => {
     useEffect(() => {
-        // 创建 Google CSE 脚本标签
         const script = document.createElement('script');
         script.src = "https://cse.google.com/cse.js?cx=02d8eddf1501844d2";
         script.async = true;
         document.body.appendChild(script);
 
-        // 设置 MutationObserver 来监听 DOM 变化
+        const debounce = (func, delay) => {
+            let timer;
+            return function(...args) {
+                if (timer) clearTimeout(timer);
+                timer = setTimeout(() => {
+                    func.apply(this, args);
+                }, delay);
+            };
+        };
+
         const observer = new MutationObserver((mutations) => {
             mutations.forEach((mutation) => {
                 mutation.addedNodes.forEach(node => {
-                    // 检查是否是搜索输入框
                     if (node.nodeName === 'INPUT' && node.classList.contains('gsc-input')) {
-                        // 设置输入事件监听器
-                        node.addEventListener('input', (event) => {
-                            // 将搜索词发送到 Google Analytics
+                        const debouncedInputEvent = debounce((event) => {
                             const searchTerm = event.target.value;
                             if (searchTerm) {
                                 gtag('event', 'search', {
                                     search_term: searchTerm,
                                 });
                             }
-                        });
+                        }, 500); // 500 ms 的延迟
+
+                        node.addEventListener('input', debouncedInputEvent);
                     }
                 });
             });
         });
 
-        // 开始监听 document.body 的子元素变化
         observer.observe(document.body, { childList: true, subtree: true });
 
-        // 组件卸载时清理
         return () => {
             document.body.removeChild(script);
             observer.disconnect();
