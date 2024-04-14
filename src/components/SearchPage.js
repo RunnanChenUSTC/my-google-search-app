bc734203846f37c1ef54bd227b130dcb8d6f0685
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-04-15 00:15:54 +0800

    Update SearchPage.js

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,36 +2,30 @@ import React, { useEffect } from 'react';
 
 const SearchPage = () => {
     useEffect(() => {
-        const script = document.createElement('script');
-        script.src = "https://cse.google.com/cse.js?cx=02d8eddf1501844d2";
-        script.async = true;
-        document.body.appendChild(script);
-
         const observer = new MutationObserver((mutations) => {
             mutations.forEach((mutation) => {
                 mutation.addedNodes.forEach(node => {
-                    // 检查是否是搜索建议容器
-                    if (node.nodeName === 'TABLE' && node.classList.contains('gssb_c')) {
-                        // 给搜索建议的每一项添加点击事件监听器
-                        node.querySelectorAll('td.gssb_a').forEach(suggestion => {
-                            suggestion.addEventListener('click', () => {
-                                const searchText = suggestion.innerText;
-                                // 发送搜索建议到 Google Analytics
-                                gtag('event', 'autocomplete_click', {
-                                    search_term: searchText,
-                                });
+                    if (node.nodeName === 'IFRAME' && node.name.startsWith('master-')) {
+                        const srcURL = new URL(node.src);
+                        const queryParam = srcURL.searchParams.get('query');
+                        if (queryParam) {
+                            // 发送搜索词到 Google Analytics
+                            gtag('event', 'search', {
+                                search_term: queryParam,
                             });
-                        });
+                        }
                     }
                 });
             });
         });
 
-        // 监听可能包含搜索建议的容器
-        observer.observe(document.body, { childList: true, subtree: true });
+        // 监听含 iframe 的容器
+        const container = document.querySelector('.gsc-wrapper');
+        if (container) {
+            observer.observe(container, { childList: true, subtree: true });
+        }
 
         return () => {
-            document.body.removeChild(script);
             observer.disconnect();
         };
     }, []);
