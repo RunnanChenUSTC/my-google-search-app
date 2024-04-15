dabd1b2ced6e606d447bfacccbc21ed4d6f458cb
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-04-15 12:56:26 +0000

    clean

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './_app';
-
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
     <React.StrictMode>
         <App />
