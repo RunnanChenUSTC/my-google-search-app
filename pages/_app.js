ec268e92ebc6abb79bb23b18f8abc541d808bfac
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-04-15 12:52:41 +0000

    fix1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchPage from './src/components/SearchPage';
+import SearchPage from '../src/components/SearchPage';
 
 const App = () => {
     return <SearchPage />;
