75540535e5b7848af9ae0b7da6fe989f14e193d8
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-04-15 19:50:54 +0800

    Update SearchPage.js

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -48,6 +48,22 @@ const SearchPage = () => {
         // .catch(error => console.error('Error posting search query:', error));
       }
     };
+      // 创建点击事件监听器
+        const clickListener = (event) => {
+            // 检查是否点击了搜索结果链接
+            if (event.target && event.target.matches("a.gs-title")) {
+                const url = event.target.href; // 获取链接的URL
+
+                // 使用gtag发送点击事件到Google Analytics
+                gtag('event', 'select_content', {
+                    content_type: 'search_result',
+                    item_id: url,
+                });
+            }
+        };
+
+        // 添加事件监听器到文档
+        document.addEventListener('click', clickListener);
 
     // 设置定时检查以捕获搜索词
     const intervalId = setInterval(getSearchQuery, 5000); // 每5秒检查一次
@@ -56,6 +72,8 @@ const SearchPage = () => {
 
     return () => {
       clearInterval(intervalId); // 清除定时器
+      document.body.removeChild(script);
+      document.removeEventListener('click', clickListener);
     };
   }, []);
 
